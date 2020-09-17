d5c7243fd3e02a424bc1edcf9e91bed20e4d8af9
Lucas Barros <lucasmello.barros@gmail.com>
2020-09-17 10:58:15 -0400

    add feature that add/removes style of list of task being dragged

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,6 +200,7 @@ var dropZoneDragHandler = function (event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -224,11 +225,21 @@ var dropTaskHandler = function (event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute("style");
+
   dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
